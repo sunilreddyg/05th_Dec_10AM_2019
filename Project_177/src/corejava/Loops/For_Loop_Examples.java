31e381992cc81a24b0419477bd2489d3aef4e8c1
Administrator <Administrator@192.168.1.24>
2020-02-12 12:47:08 +0530

    Arrays

---
+++ Project_177/src/corejava/Loops/For_Loop_Examples.java
@@ -0,0 +1,46 @@
+package corejava.Loops;
+
+public class For_Loop_Examples {
+
+	public static void main(String[] args) 
+	{
+		/*
+		 * Example:--> Print number 1 to 10
+		 */
+		for (int i = 1; i <=10; i++) 
+		{
+			System.out.println(i);
+		}
+		
+		//Print number 10 to 1 using decrement
+		for (int i = 10; i >=1 ; i--) 
+		{
+			System.out.println(i);
+		}
+				
+		//How to reverse a string
+		String toolname="webdriver";
+		char[] c=toolname.toCharArray();
+		
+		for (int i = c.length-1; i >=0; i--) 
+		{
+			char ch=toolname.charAt(i);
+			System.out.print(ch);
+		}
+		
+		System.out.println("\n");
+		
+		
+		
+		//Array values to iterate using for-loop
+		String tools[]= {"IDE","RC","WD","GRID"};
+		//Iterate for number of array length
+		for (int i = 0; i < tools.length; i++) 
+		{
+			System.out.println(tools[i]);
+		}
+		
+		
+	}
+
+}
